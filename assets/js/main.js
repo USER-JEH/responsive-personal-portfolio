31b488202e9801a02033f5e640e972f3fa9cbd72
USER-JEH <han880330@yahoo.co.jp>
2021-06-19 09:27:30 +0900

    20210619

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,10 +29,42 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementByClassName('skills__content'),
+			skillsHeader = document.querySelectorAll('.skills__header')
 
+function togglesSkills() {
+	let itemClass = this.parentNode.className
+	
+	for (i = 0; i < skillsContent.length; i++) {
+		skillsContent[i].className= 'skills__content skills__close'
+	}
+	if (itemClass === 'skills__content skills__close') {
+		this.parentNode.className = 'skills__content skills__open'
+	}
+}
 
+skillsHeader.forEach((el) => {
+	el.addEventListener('click', togglesSkills)
+})
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+	tabContents = document.querySelectorAll('[data-content]')
+			
+tabs.forEach(tab => {
+	tab.addEventListener('click', () => {
+		const target = document.querySelector(tab.dataset.target)
 
+		tabContents.forEach(tabContents => {
+			tabContents.classList.remove('qualification__active')
+		})
+		target.classList.add('qualification__active')
+
+		tabs.forEach(tab => {
+			tab.classList.remove('qualification__active')
+		})
+		tab.classList.add('qualification__active')
+	})
+}) 
 
 /*==================== SERVICES MODAL ====================*/
 
