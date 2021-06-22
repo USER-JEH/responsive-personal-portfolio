c54ea7805b5c1652c5ee6307e5e04d5679810a05
USER-JEH <han880330@yahoo.co.jp>
2021-06-22 17:06:42 +0900

    service&portfolio part

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,7 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementByClassName('skills__content'),
+const skillsContent = document.getElementsByClassName('skills__content'),
 			skillsHeader = document.querySelectorAll('.skills__header')
 
 function togglesSkills() {
@@ -67,10 +67,41 @@ tabs.forEach(tab => {
 }) 
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+			modalBtns = document.querySelectorAll('.services__button'),
+			modalCloses = document.querySelectorAll('.services__modal-close')
 
+let modal = function (modalClick) {
+	modalViews[modalClick].classList.add('active-modal')
+}
 
+modalBtns.forEach((modalBtn, i) => {
+	modalBtn.addEventListener('click', () => {
+		modal(i)
+	})
+})
+
+modalCloses.forEach((modalClose) => {
+	modalClose.addEventListener('click', () => {
+		modalViews.forEach((modalView) => {
+			modalView.classList.remove('active-modal')
+		})
+	})
+})
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper('.portfolio__container', {
+	cssMode: true,
+	loop:true,
 
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+});
 
 /*==================== TESTIMONIAL ====================*/
 
